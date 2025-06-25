66e2a285669fa0161c9bbf025b23d52d208de76b
dmytrolesyk <dmitriylesik@gmail.com>
2025-06-25 14:57:21 +0300

    refactor testids into constants

--- tests/e2e/tracks.test.ts
+++ tests/e2e/tracks.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../fixtures/with-backend.fixture';
+import { TEST_IDS } from '../constants';
 
 test('has title and header', async ({ page }) => {
   await page.goto('/tracks');
@@ -10,44 +11,45 @@ test('has title and header', async ({ page }) => {
 
 test('setting number of items in URL works correctly', async ({ page }) => {
   await page.goto('/tracks?page=1&size=10');
-  await expect(page.getByTestId(/^track-item-\d+$/)).toHaveCount(10);
+  await expect(page.getByTestId(TEST_IDS.TRACK_ITEM)).toHaveCount(10);
 
   await page.goto('/tracks?page=1&size=20');
-  await expect(page.getByTestId(/^track-item-\d+$/)).toHaveCount(20);
+  await expect(page.getByTestId(TEST_IDS.TRACK_ITEM)).toHaveCount(20);
 
   await page.goto('/tracks?page=1&size=50');
-  await expect(page.getByTestId(/^track-item-\d+$/)).toHaveCount(50);
+  await expect(page.getByTestId(TEST_IDS.TRACK_ITEM)).toHaveCount(50);
 });
 
 test('search works correctly', async ({ page }) => {
   await page.goto('/tracks');
-  await page.getByTestId('search-input').click();
-  await page.getByTestId('search-input').fill('rolling');
-  await expect(page.getByTestId(/^track-item-\d+-title$/)).toHaveCount(1);
-  const trackTitle = await page.getByTestId(/^track-item-\d+-title$/).textContent();
+  await page.waitForTimeout(600); // search input is debounced
+  await page.getByTestId(TEST_IDS.SEARCH_INPUT).click();
+  await page.getByTestId(TEST_IDS.SEARCH_INPUT).fill('rolling');
+  await expect(page.getByTestId(TEST_IDS.TRACK_TITLE)).toHaveCount(1);
+  const trackTitle = await page.getByTestId(TEST_IDS.TRACK_TITLE).textContent();
   expect(trackTitle).toBe('Like a Rolling Stone');
 });
 
 test('add track functionality works correctly', async ({ page }) => {
   await page.goto('/tracks');
-  await page.getByTestId('create-track-button').click();
+  await page.getByTestId(TEST_IDS.CREATE_TRACK_BUTTON).click();
   await expect(page.getByRole('dialog')).toBeVisible();
 
-  await page.getByTestId('input-title').fill('Custer');
-  await page.getByTestId('input-artist').click();
-  await page.getByTestId('input-artist').fill('Slipknot');
-  await page.getByTestId('input-album').click();
-  await page.getByTestId('input-album').fill('The gray chapter');
-  await page.getByTestId('input-album').click();
-  await page.getByTestId('input-cover-image').click();
+  await page.getByTestId(TEST_IDS.INPUT_TITLE).click();
+  await page.getByTestId(TEST_IDS.INPUT_TITLE).fill('Custer');
+  await page.getByTestId(TEST_IDS.INPUT_ARTIST).click();
+  await page.getByTestId(TEST_IDS.INPUT_ARTIST).fill('Slipknot');
+  await page.getByTestId(TEST_IDS.INPUT_ALBUM).click();
+  await page.getByTestId(TEST_IDS.INPUT_ALBUM).fill('The gray chapter');
+  await page.getByTestId(TEST_IDS.INPUT_COVER_IMAGE).click();
   await page
-    .locator('[data-testid="input-cover-image"]')
+    .getByTestId(TEST_IDS.INPUT_COVER_IMAGE)
     .fill('https://upload.wikimedia.org/wikipedia/en/3/36/5_The_Gray_Chapter_Artwork.jpg');
   await page.getByRole('combobox').click();
   await page.getByRole('option', { name: 'Metal' }).click();
   await page.getByRole('combobox').click();
   await page.getByRole('option', { name: 'Rock' }).click();
-  await page.locator('[data-testid="submit-button"]').click();
+  await page.getByTestId(TEST_IDS.SUBMIT_BUTTON).click();
 
   const track = page.locator('tr[data-testid^=track-item-]', {
     hasText: 'Custer',
@@ -64,16 +66,15 @@ test('edit functionality works correctly', async ({ page }) => {
     .click();
   await expect(page.getByRole('dialog')).toBeVisible();
 
-  await page.getByTestId('input-title').fill('Edited title');
-  await page.getByTestId('input-artist').click();
-  await page.getByTestId('input-artist').fill('Edited artist');
-  await page.getByTestId('input-album').click();
-  await page.getByTestId('input-album').fill('Edited album');
+  await page.getByTestId(TEST_IDS.INPUT_TITLE).click();
+  await page.getByTestId(TEST_IDS.INPUT_TITLE).fill('Edited title');
+  await page.getByTestId(TEST_IDS.INPUT_ARTIST).click();
+  await page.getByTestId(TEST_IDS.INPUT_ARTIST).fill('Edited artist');
+  await page.getByTestId(TEST_IDS.INPUT_ALBUM).click();
+  await page.getByTestId(TEST_IDS.INPUT_ALBUM).fill('Edited album');
   await page.getByRole('combobox').click();
-  await page.getByRole('option', { name: 'Jazz' }).click();
-  await page.getByRole('combobox').click();
-  await page.getByRole('option', { name: 'Folk' }).click();
-  await page.getByTestId('submit-button').click();
+  await page.getByRole('option').first().click();
+  await page.getByTestId(TEST_IDS.SUBMIT_BUTTON).click();
 
   const track = page.locator('tr[data-testid^=track-item-]', {
     hasText: 'Edited title',
@@ -81,23 +82,23 @@ test('edit functionality works correctly', async ({ page }) => {
 
   await expect(track).toBeVisible();
 
-  await expect(track.getByTestId(/^track-item-\d+-title$/)).toHaveText('Edited title');
-  await expect(track.getByTestId(/^track-item-\d+-artist$/)).toHaveText('Edited artist');
-  await expect(track.getByTestId(/^track-item-\d+-album$/)).toHaveText('Edited album');
+  await expect(track.getByTestId(TEST_IDS.TRACK_TITLE)).toHaveText('Edited title');
+  await expect(track.getByTestId(TEST_IDS.TRACK_ARTIST)).toHaveText('Edited artist');
+  await expect(track.getByTestId(TEST_IDS.TRACK_ALBUM)).toHaveText('Edited album');
 });
 
 test('delete functionality works correctly', async ({ page }) => {
   await page.goto('/tracks');
 
-  const track = page.getByTestId(/^track-item-\d+$/).first();
+  const track = page.getByTestId(TEST_IDS.TRACK_ITEM).first();
   const trackTestId = await track.getAttribute('data-testid');
 
   // track Locator.prototype.getAttribute can return null
   expect(trackTestId).toBeTruthy();
 
-  await track.getByTestId(/delete-track-/).click();
+  await track.getByTestId(TEST_IDS.DELETE_TRACK).click();
   await expect(page.getByRole('alertdialog')).toBeVisible();
-  await page.getByTestId('confirm-delete').click();
+  await page.getByTestId(TEST_IDS.CONFIRM_DELETE).click();
   await expect(page.getByRole('alertdialog')).not.toBeVisible();
 
   await expect(page.getByTestId(trackTestId as string)).toHaveCount(0);
