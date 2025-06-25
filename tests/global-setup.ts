2c528a592e798a42e7c46f0abcf6846d8e5b2351
dmytrolesyk <dmitriylesik@gmail.com>
2025-06-25 13:40:20 +0300

    change seed command

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -8,7 +8,7 @@ async function globalSetup() {
   try {
     backendContainer = await getBackendContainer();
 
-    await backendContainer.exec(['node dist/seed.js']);
+    await backendContainer.exec(['node', 'dist/seed.js']);
 
     const backendPort = backendContainer.getMappedPort(8000);
     const backendHost = backendContainer.getHost();
